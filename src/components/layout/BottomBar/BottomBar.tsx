709a3a15ba5f12e154cbb93318076cea1881d948
Jakub Kozik <jakubkozik120@gmail.com>
2023-04-13 22:47:53 +0200

    Fix lint error

--- src/components/layout/BottomBar/BottomBar.tsx
+++ src/components/layout/BottomBar/BottomBar.tsx
@@ -1,7 +1,5 @@
-import React from 'react';
 import styles from './BottomBar.module.scss';
 import CoinSelector from '../../features/CoinSelector/CoinSelector';
-import BetList from '../../common/PayoutsList/PayoutsList';
 import Roulette from '../../features/Roulette/Roulette';
 import BalanceAndBet from '../../common/BalanceAndBet/BalanceAndBet';
 
