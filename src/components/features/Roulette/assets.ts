55dbf983ee59fb4784f31114116143078a78faf0
Jakub Kozik <jakubkozik120@gmail.com>
2023-04-20 22:08:05 +0200

    Fix errors

--- src/components/features/Roulette/assets.ts
+++ src/components/features/Roulette/assets.ts
@@ -1,12 +1,12 @@
 import { Assets, Texture } from 'pixi.js';
 
 export async function assetsManager() {
-  Assets.add('layer1', 'http://localhost:3000/assets/wheel/roulette_1.png');
-  Assets.add('layer2', 'http://localhost:3000/assets/wheel/roulette_2.png');
-  Assets.add('layer3', 'http://localhost:3000/assets/wheel/roulette_3.png');
-  Assets.add('layer4', 'http://localhost:3000/assets/wheel/roulette_4.png');
-  Assets.add('layer5', 'http://localhost:3000/assets/wheel/roulette_5.png');
-  Assets.add('ball', 'http://localhost:3000/assets/wheel/ball.png');
+  Assets.add('layer1', '/assets/wheel/roulette_1.png');
+  Assets.add('layer2', '/assets/wheel/roulette_2.png');
+  Assets.add('layer3', '/assets/wheel/roulette_3.png');
+  Assets.add('layer4', '/assets/wheel/roulette_4.png');
+  Assets.add('layer5', '/assets/wheel/roulette_5.png');
+  Assets.add('ball', '/assets/wheel/ball.png');
 
   return await Assets.load<Texture>([
     'ball',
