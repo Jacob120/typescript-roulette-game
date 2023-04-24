e5c4537b2b90adf1fd366c69c095b09a8559fd1f
Jakub Kozik <jakubkozik120@gmail.com>
2023-04-24 16:24:17 +0200

    Fix lint error

--- src/components/layout/TopBar/TopBar.tsx
+++ src/components/layout/TopBar/TopBar.tsx
@@ -3,16 +3,12 @@ import Logo from '../../common/Logo/Logo';
 import PayoutsList from '../../common/PayoutsList/PayoutsList';
 import styles from './TopBar.module.scss';
 import WinningNumbers from '../../features/WinningNumbers/WinningNumbers';
-import { useWindowDimensions } from '../../../utils/useWindowDimensions';
 
 interface TopBarProps {
   isListVisible: boolean;
 }
 
 const TopBar: React.FC<TopBarProps> = ({ isListVisible }) => {
-  const { width } = useWindowDimensions();
-  const [isVertical, setIsVertical] = useState(false);
-
   return (
     <div className=''>
       <div className={styles.wrapper}>
